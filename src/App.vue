e2a56da49c830e5fc1d4baec71f5a5ceefe1e3e7
ajomuch92 <aaronmontes_92@yahoo.es>
2021-11-21 19:44:10 -0600

    feat: 🎸 Creating overlay component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
     <router-view />
+    <overlay v-show="showOverlay">
+      <vk-spinner class="spinner" ratio="3.0" />
+    </overlay>
+    <vk-notification position="top-right" :messages.sync="messages" />
   </div>
 </template>
 
 <script>
+import Overlay from './components/overlay.vue'
 export default {
   name: 'App',
   components: {
+    Overlay
+  },
+  data: () => ({
+    showOverlay: false,
+    messages: []
+  }),
+  created() {
+    this.$bus.$on('loading', this.overlayHandler);
+    this.$bus.$on('notification', this.messagesHandler);
+  },
+  beforeDestroy() {
+    this.$bus.$off('loading', this.overlayHandler);
+    this.$bus.$off('notification', this.messagesHandler);
+  },
+  methods: {
+    overlayHandler(value) {
+      this.showOverlay = value ? true : false;
+    },
+    messagesHandler(value) {
+      this.messages.push(value)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
---
+++ src/components/overlay.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="overlay a-flex a-center a-align-items-center">
+    <slot name="default" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Overlay'
+}
+</script>
+
+<style scoped>
+  .overlay {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.2);
+    z-index: 1200;
+  }
+</style>
