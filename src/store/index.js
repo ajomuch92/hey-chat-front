86875a3e55c26c27aac5ed9ed59679a46e199150
ajomuch92 <aaronmontes_92@yahoo.es>
2021-11-21 19:43:38 -0600

    feat: 🎸 Adding local storage secure

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import * as SecureLS from 'secure-ls';
+
+const ls = new SecureLS({encodingType: 'aes', isCompression: false});
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-
+  state: {
+    accessToken: ''
+  },
+  mutations: {
+    setAccessToken(state, token) {
+      state.accessToken = token;
+    }
+  },
+  getters: {
+    accessToken: (state) => state.accessToken
+  }
 })
 
 export default store;
+
+store.subscribe((mutation, state) => {
+	ls.set('hey-chat', state);
+});
