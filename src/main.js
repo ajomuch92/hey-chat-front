dd1bc197e173019722179886892cc6f51809a9f3
ajomuch92 <aaronmontes_92@yahoo.es>
2021-11-24 20:38:01 -0600

    fix: 🐛 Adding sweetalert library to project

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './routes';
 import Vuikit from 'vuikit';
 import VuikitIcons from '@vuikit/icons';
 import store from './store';
-import 'a-flexbox/a-flexbox.css';
-import './styles/style.css';
 import _ from 'lodash';
 import VueGoogleCharts from 'vue-google-charts';
+import VueSweetalert2 from 'vue-sweetalert2';
 
+import 'a-flexbox/a-flexbox.css';
+import './styles/style.css';
+import 'sweetalert2/dist/sweetalert2.min.css';
 import '@vuikit/theme'
 
 Vue.config.productionTip = false
@@ -16,6 +18,7 @@ Vue.config.productionTip = false
 Vue.use(Vuikit);
 Vue.use(VuikitIcons);
 Vue.use(VueGoogleCharts);
+Vue.use(VueSweetalert2);
 
 Vue.prototype._ = _;
 Vue.prototype.$bus = new Vue();
